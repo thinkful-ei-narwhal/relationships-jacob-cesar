a8870e62f3721384eae3c400f7eccea4124d914b
Cesar Morales <cesarlenin2@live.com>
2020-05-11 09:01:55 -0700

    added questions

---
+++ queries.sql
@@ -0,0 +1,10 @@
+
+    -- How many people work in the Sales department?
+
+    -- List the names of all employees assigned to the 'Plan Christmas party' project.
+
+    -- List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
+
+    -- Which projects are the Sales department employees assigned to?
+    
+    -- List only the managers that are assigned to the 'Watch paint dry' project.
